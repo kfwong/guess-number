d0b6b5968c2584542ffd558712dea9cfc884c290
WONG KANG FEI <kangfei.wong@thoughtworks.com>
2019-07-15 11:55:44 +0800

    [KF|lanlan] Added more logic for A

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,28 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Game {
+
+    String answer;
+
+    public void start(InputStream in, OutputStream out){
+        /*
+
+        1. System generate a random number
+        2. User input a guess number
+        3. System check the guess number against the answer
+        4. System output XAXB format to the user
+        5. repeat steps 2-4 until 4A4B.
+         */
+
+        answer = generateRandomFourNumbers();
+
+
+
+    }
 
     public boolean isNumber(String data){
         try {
@@ -18,6 +42,36 @@ public class Game {
             if(data.indexOf(a) >= 0){
                 count ++;
             };
+        }
+
+        return count;
+    }
+
+    public String generateRandomFourNumbers() {
+        Random random = new Random();
+
+        Set<Integer> buckets = new HashSet<>();
+
+        do{
+            buckets.add(random.nextInt(9));
+        }while(buckets.size() < 4);
+
+        String result = "";
+
+        for (int integer: buckets){
+            result += integer;
+        }
+
+        return result;
+    }
+
+    public int correctNumberAndPosition(String guess, String answer) {
+        int count = 0;
+
+        for(int i=0; i<answer.length(); i++){
+            if(guess.toCharArray()[i] == answer.toCharArray()[i]){
+                count++;
+            }
         }
 
         return count;
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,10 @@
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.TestCase.*;
 
@@ -36,15 +40,54 @@ public class MainTest {
 
 
     @Test
-    public void should_have_one_digit_in_correct_position(){
-        String data = "1234";
+    public void should_have_one_digit_is_correct_number(){
+        String guess = "1234";
         String answer = "5167";
 
-        int correctNumberCount = game.correctNumberCount(data, answer);
+        int correctNumberCount = game.correctNumberCount(guess, answer);
 
 
         assertEquals(correctNumberCount, 1);
     }
+
+    @Test
+    public void should_return_four_digits_that_is_not_repeated(){
+        String data = game.generateRandomFourNumbers();
+
+        assertEquals(data.length(), 4);
+
+        Set<Character> buckets = new HashSet<>();
+
+        for( char d: data.toCharArray()){
+            buckets.add(d);
+        }
+
+        System.out.println(data);
+
+        assertEquals(buckets.size(), 4);
+
+    }
+
+    @Test
+    public void should_have_four_correct_number_and_position(){
+        String guess = "1234";
+        String answer = "1234";
+
+        int correctNumberAndPostion = game.correctNumberAndPosition(guess, answer);
+
+        assertEquals(4, correctNumberAndPostion);
+    }
+
+    @Test
+    public void should_have_two_correct_number_and_position() {
+        String guess = "1234";
+        String answer = "1290";
+
+        int correctNumberAndPosition = game.correctNumberAndPosition(guess, answer);
+
+        assertEquals(2, correctNumberAndPosition);
+    }
+
 }
 
 
